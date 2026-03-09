8aebc39ec0860f92dd1b6758bcf0447fe6745ebb
Roma <romanyushchyk2006@gmail.com>
2026-03-09 11:03:46 +0200

    feat: add swagger ui for api tests

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,16 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Snow Shop API')
+    .setDescription('The Snowboard Shop API description')
+    .setVersion('1.0')
+    .addTag('products')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(4200);
 }
--- server/src/products/dto/create-product.dto.ts
+++ server/src/products/dto/create-product.dto.ts
@@ -1,8 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 export class CreateProductDto {
+  @ApiProperty({ example: 'Snowboard X', description: 'Назва товару' })
   title: string;
+  @ApiProperty({ example: 'Burton', description: 'Бренд' })
   brand: string;
+  @ApiProperty({ example: 500, description: 'Ціна в доларах' })
   price: number;
+  @ApiProperty({ example: '/img/board.jpg', description: 'Посилання на фото' })
   image: string;
+  @ApiProperty({ example: ['New', 'Sale'], description: 'Бейджики' })
   badges: string[];
+  @ApiProperty({
+    example: ['Wood Core', 'Flex 5'],
+    description: 'Характеристики',
+  })
   specs: string[];
 }
